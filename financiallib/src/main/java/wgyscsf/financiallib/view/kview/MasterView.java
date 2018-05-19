a1117fe92a1bb865ce69ce843355ad7b17084589
wgyscsf <wgyscsf@163.com>
2018-05-19 18:49:46 +0800

    Merge branch 'master' into feature_v0.0.3
    # Conflicts:
    #	financiallib/src/main/java/wgyscsf/financiallib/view/kview/MasterView.java

--- financiallib/src/main/java/wgyscsf/financiallib/view/kview/MasterView.java
+++ financiallib/src/main/java/wgyscsf/financiallib/view/kview/MasterView.java
@@ -10,7 +10,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.GestureDetectorCompat;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
